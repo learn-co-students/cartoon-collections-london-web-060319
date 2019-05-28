7e9f65e5c54bee21671b40317578224ce6f1b960
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-28 16:10:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves = ["Dopey", "Grumpy", "Bashful"]
+  indexplusone = index + 1 
+  dwarves.each_with_index do |value, indexplusone|
+    puts "#{indexplusone}. #{value}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+    veggies = ["carrot", "cucumber", "pepper"]
+    veggies.split.map(&: capitalize)
 end
 
+def summon_captain_planet(veggies)
+  veggies.collect do |x|
+    x << "!"
+  end 
 def long_planeteer_calls# code an argument here
   # Your code here
 end
